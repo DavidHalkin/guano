75aadad38bad8b3c1323ebaa823775e9b9a2489a
David <northfire@mail.ru>
2017-07-27 16:04:57 +0300

    closed faking project

--- gulpfile.js
+++ gulpfile.js
@@ -87,8 +87,7 @@ gulp.task('sprite', function () {
 gulp.task('scripts', function() {
 	return gulp.src([ // Берем все необходимые библиотеки
 		'bower_components/jquery/dist/jquery.min.js', 
-		'bower_components/owl.carousel/dist/owl.carousel.min.js',
-		'bower_components/matchHeight/dist/jquery.matchHeight-min.js'
+		'bower_components/owl.carousel/dist/owl.carousel.min.js'
 		])
 		// .pipe(concat('libs.min.js')) // Собираем их в кучу в новом файле libs.min.js
 		// .pipe(uglify()) // Сжимаем JS файл
